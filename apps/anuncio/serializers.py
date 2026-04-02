3c81afb2ea0697ede551f628a2f75c691ed896d8
Fantitom03 <leivafrancisco2017@gmail.com>
2026-04-01 21:20:59 -0300

    Nuevo serializador para Categorías utilizando serializers.Serializer que será utilizado solo para los anuncios

--- apps/anuncio/serializers.py
+++ apps/anuncio/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Anuncio, Categoria
+from apps.anuncio.models import Anuncio, Categoria
 from apps.usuario.models import Usuario
 
 class CategoriaSerializer(serializers.ModelSerializer): 
@@ -10,78 +10,58 @@ class CategoriaSerializer(serializers.ModelSerializer):
             'nombre',
             'activa'
         ]
-        extra_kwargs = {
-            'nombre': {
-                'validators': [], 
-            }
-        }
+
+
+
+# Heredamos de Serializer (no ModelSerializer). 
+# Solo definimos los campos que esperamos recibir. Es decir, no hay validación de DB, ni de duplicados, ni nada. Solo validación de tipos y formatos.
+class CategoriaAnidadaSerializer(serializers.Serializer):
+    id = serializers.IntegerField(required=False)
+    nombre = serializers.CharField(max_length=100)
+    activa = serializers.BooleanField(default=True, required=False)
+
+
 
 class AnuncioSerializer(serializers.ModelSerializer):
-    categorias = CategoriaSerializer(many=True)
+    # Usamos el serializador "puro"
+    categorias = CategoriaAnidadaSerializer(many=True, required=False)
+    
     class Meta:
         model= Anuncio
         fields = [
-            'id',
-            'titulo',
-            'descripcion',
-            'precio_inicial',
-            'imagen',
-            'fecha_inicio',
-            'fecha_fin',
-            'activo',
-            'categorias',
-            'publicado_por',
-            'oferta_ganadora'
+            'id', 'titulo', 'descripcion', 'precio_inicial', 'imagen',
+            'fecha_inicio', 'fecha_fin', 'activo', 'categorias',
+            'publicado_por', 'oferta_ganadora'
         ]
         read_only_fields = ['publicado_por', 'oferta_ganadora']
 
+
     def create(self, validated_data):
-        # Sacamos las categorías del diccionario de datos validados
-        categorias_data = validated_data.pop('categorias')
-        
-        # Creamos el Anuncio con el resto de los datos (título, precio, etc.)
+        categorias_data = validated_data.pop('categorias', [])
         anuncio = Anuncio.objects.create(**validated_data)
         
-        # Procesamos cada categoría
         for categoria_data in categorias_data:
             nombre_categoria = categoria_data.get('nombre')
-            
-            # get_or_create devuelve una tupla: (el_objeto, fue_creado_booleano)
-            # Usamos "_" para ignorar el booleano porque solo nos importa el objeto
             if nombre_categoria:
+                # Como la validación de duplicados no frenó la petición, puedo ejecutar esto
                 categoria, _ = Categoria.objects.get_or_create(nombre=nombre_categoria)
-                
-                # Agregamos la categoría a la relación del Anuncio
                 anuncio.categorias.add(categoria)
                 
         return anuncio
-    
-    def update(self, instance, validated_data):
-        categorias_data = validated_data.pop('categorias', []) #Se toma solo lo ingresado en categorias
 
-        #Se instancian los demas elementos, como sean nombre, descripcion, etc.
+    def update(self, instance, validated_data):
+        categorias_data = validated_data.pop('categorias', []) 
+
         for attr, value in validated_data.items():
             setattr(instance, attr, value)
         instance.save()
 
-        #Almacenará las nuevas categorias
         nuevas_categorias = []
-
-        #Recorremos cada categoria ingresada
         for categoria_data in categorias_data:
-            categoria_id = categoria_data.get('id') #Se obtiene el id
-            nombre_categoria = categoria_data.get('nombre') #Se obtiene el nombre
-            if categoria_id: #En caso de que exista ID
-                try:
-                    categoria = Categoria.objects.get(id=categoria_id) #Obtener el objeto categoria por ID
-                except Categoria.DoesNotExist:
-                    categoria, _ = Categoria.objects.get_or_create(nombre=nombre_categoria) #Si es un ID invalido, hay que obtener el objeto categoria por nombre
-            else:
-                categoria, _ = Categoria.objects.get_or_create(nombre=nombre_categoria) #Obtener el objeto categoria por nombre
-            
-            nuevas_categorias.append(categoria) #Agregamos la "nueva" categoria
+            nombre_categoria = categoria_data.get('nombre')
+            if nombre_categoria:
+                categoria, _ = Categoria.objects.get_or_create(nombre=nombre_categoria)
+                nuevas_categorias.append(categoria)
 
-        instance.categorias.set(nuevas_categorias) #Las seteamos en la instancia de anuncios
-
-        return instance #retornamos el anuncio
-        
+        instance.categorias.set(nuevas_categorias) 
+        return instance
